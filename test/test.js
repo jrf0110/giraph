579aa326dd354c7e38b51680c7686aca0989498c
John Fawcett <jrf0110@gmail.com>
2015-05-26 20:44:21 -0500

    simpler data model, faster stuff and stuff

--- test/test.js
+++ test/test.js
@@ -29,6 +29,36 @@ describe('Giraph', function(){
       assert.equal( graph.map.b, undefined, 'immutability test failed' );
     });
 
+    it('.connect(a,b)', function(){
+      var g1 = graph
+        .add('a')
+        .add('b')
+        .connect('a', 'b');
+
+      assert.deepEqual( g1.get('a').edges, {
+        b: undefined
+      });
+
+      assert.deepEqual( g1.get('b').edges, {
+        a: undefined
+      });
+    });
+
+    it('.connect(a,b,[weight])', function(){
+      var g1 = graph
+        .add('a')
+        .add('b')
+        .connect('a', 'b', 3);
+
+      assert.deepEqual( g1.get('a').edges, {
+        b: 3
+      });
+
+      assert.deepEqual( g1.get('b').edges, {
+        a: 3
+      });
+    });
+
     it('.remove(id)', function(){
       assert.equal(
         graph.add('a').remove('a').get('a'), undefined
@@ -48,8 +78,8 @@ describe('Giraph', function(){
         .remove('a');
 
       assert.equal( g.map.a, undefined );
-      assert.equal( g.map.d.incoming.a, undefined );
-      assert.equal( g.map.d.outgoing.a, undefined );
+      assert.equal( g.map.d.edges.a, undefined );
+      assert.equal( g.map.d.edges.a, undefined );
     });
 
     it('general immutability', function(){
@@ -87,34 +117,5 @@ describe('Giraph', function(){
       var vc = v.clone();
       assert.notEqual( v.data, vc.data );
     });
-
-    // it('.connect(vertex b) - connects to vertices', function(){
-    //   var va = giraph.vertex('a');
-    //   var vb = giraph.vertex('b');
-
-    //   var vbconnected = vb.connect( va );
-
-    //   // Assert immutability
-    //   assert( vb !== vbconnected );
-    //   assert.equal( vb.outgoing.a, undefined );
-
-    //   assert( vbconnected.outgoing.a === va );
-    //   assert( va.incoming.b === vbconnected );
-    // });
-
-    // it('.del() - cleans up paths', function(){
-    //   var va = giraph.vertex('a');
-    //   var vb = giraph.vertex('b').connect( va );
-
-    //   assert( vbconnected.outgoing.a === va );
-    //   assert( va.incoming.b === vbconnected );
-
-    //   va.del();
-
-    //   assert( vbconnected.outgoing.a === undefined );
-    //   assert( va.incoming.b === undefined );
-
-    //   assert.eq
-    // });
   });
 });
